cd5600b1e837943b88368a459cd37e09599283e4
ArtDub <rusartdub@gmail.com>
2020-09-07 13:55:21 +0300

    week4 routing complete

---
+++ web_python/week4/coursera_blog/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/art/study/courses/web_python/week4/coursera_blog/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
