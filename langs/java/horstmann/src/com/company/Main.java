95a8c7254f7cc48cc541080c7fbf1d862dc61ff4
Artyom Dubinin <rusartdub@gmail.com>
2021-10-31 11:33:11 +0300

    Fix dirs

---
+++ langs/java/horstmann/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
